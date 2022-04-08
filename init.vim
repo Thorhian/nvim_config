053a5929c7f25b45717af0ceb9d02dafb3db08a4
Thorhian <thorhian@gmail.com>
2022-04-08 13:55:45 -0600

    Initial Config

--- init.vim
+++ init.vim
@@ -1,3 +1,30 @@
+call plug#begin(has('nvim') ? stdpath('data') . '/plugged' : '~/.vim/plugged')
+
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'saadparwaiz1/cmp_luasnip'
+
+call plug#end()
+
+set completeopt=menu,menuone,noselect
+set nocompatible
+set showmatch
+set ignorecase
+set mouse=v
+set incsearch
+set tabstop=4
+set expandtab
+set shiftwidth=4
+set autoindent
+set relativenumber
+" set cc=80
+filetype plugin indent on
+set ttyfast
+
 lua <<EOF
 print('hello from lua')
 EOF
--- lua/basic.lua
+++ lua/basic.lua
@@ -1,4 +1,3 @@
-print('hello there from the basic script!')
 
 vim.g.mapleader = ' '
 
@@ -8,3 +7,25 @@ vim.cmd [[
 	set autochdir
 ]]
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local lspconfig = require('lspconfig')
+
+local servers = {'clangd'}
+for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+        capabilities = capabilities,
+    }
+end
+
+local luasnip
+
+vim.api.nvim_set_keymap('n', '<Leader>wl', ':wincmd l<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>wh', ':wincmd h<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>wj', ':wincmd j<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>wk', ':wincmd k<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>wv', ':wincmd v<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>wv', ':wincmd v<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>wq', ':wincmd q<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>w=', ':wincmd =<CR>', {noremap = true})
